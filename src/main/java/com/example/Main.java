6bd4ed29dc32f93e6ea57a70990b67fa9737f716
JuanOquendo23 <juanoquendoweb@gmail.com>
2024-10-15 13:13:44 -0500

    Inicio de sesion

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,28 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        String username = "Admin";
+        String password = "12345";
+
+        System.out.println("Ingrese el usuario: ");
+        String usernameinput = input.nextLine();
+
+        System.out.println("Ingrese la contraseña: ");
+        String passwordinput = input.nextLine();
+
+        if (usernameinput.equals(username) && passwordinput.equals(password)) {
+            System.out.println("Inicio de sesion exitoso");
+
+        } else {
+            System.out.println("Error de inicio de sesion,Vuelve a intentarlo.");
+        }
+
+        input.close();
+
     }
 }
